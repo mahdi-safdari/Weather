e53b597025fc7006397df37e9edbab9c0dfd42bd
Mahdi <mti.safdari@gmail.com>
2022-12-11 10:25:09 +0330

    inject ApiProvider - Repositories - UseCase - HomeBloc

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,3 +1,7 @@
+import 'package:clean_block_floor_lint_dio/features/feature_weather/data/repository/weather_repositoryimpl.dart';
+import 'package:clean_block_floor_lint_dio/features/feature_weather/domain/repository/weather_repository.dart';
+import 'package:clean_block_floor_lint_dio/features/feature_weather/domain/use_cases/get_current_weather_usecase.dart';
+import 'package:clean_block_floor_lint_dio/features/feature_weather/presentation/bloc/home_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'features/feature_weather/data/data_source/remote/api_provider.dart';
 
@@ -5,4 +9,15 @@ GetIt locator = GetIt.instance;
 
 setup() {
   locator.registerSingleton<ApiProvider>(ApiProvider());
+
+  //! Repositories
+  locator
+      .registerSingleton<WeatherRepository>(WeatherRepositoryImpl(locator()));
+
+  //! use Case
+  locator.registerSingleton<GetCurrentWeatherUseCase>(
+      GetCurrentWeatherUseCase(locator()));
+
+  //! Bloc
+  locator.registerSingleton<HomeBloc>(HomeBloc(locator()));
 }
