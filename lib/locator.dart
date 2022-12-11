5cd1cb14a383d4d81ff4e3fa3d5aca719694a998
Mahdi <mti.safdari@gmail.com>
2022-12-11 10:40:35 +0330

    call GetIt setup on main()

--- lib/locator.dart
+++ lib/locator.dart
@@ -6,7 +6,7 @@ import 'package:get_it/get_it.dart';
 import 'features/feature_weather/data/data_source/remote/api_provider.dart';
 
 GetIt locator = GetIt.instance;
-
+//! Dependency injection with get_it
 setup() {
   //! API provider
   locator.registerSingleton<ApiProvider>(ApiProvider());
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/material.dart';
-import 'core/widgets/main_wrapper.dart';
-import 'features/feature_weather/data/data_source/remote/api_provider.dart';
-import 'features/feature_weather/data/repository/weather_repositoryimpl.dart';
 import 'features/feature_weather/domain/use_cases/get_current_weather_usecase.dart';
+import 'features/feature_weather/data/repository/weather_repositoryimpl.dart';
+import 'features/feature_weather/data/data_source/remote/api_provider.dart';
+import 'core/widgets/main_wrapper.dart';
+import 'package:flutter/material.dart';
+import 'locator.dart';
 
-void main() {
+void main() async {
+  //! Dependency injection with get_it
+  await setup();
   runApp(const MyApp());
 }
 
