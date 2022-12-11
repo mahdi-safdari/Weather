dced62a7f763f5d05175f89ae95d6fc0dd9ef5a7
Mahdi <mti.safdari@gmail.com>
2022-12-11 11:58:02 +0330

    multi bloc provider

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -1,13 +1,47 @@
+import 'package:clean_block_floor_lint_dio/features/feature_weather/presentation/bloc/cw_status.dart';
+import 'package:clean_block_floor_lint_dio/features/feature_weather/presentation/bloc/home_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MainWrapper extends StatelessWidget {
+class MainWrapper extends StatefulWidget {
   const MainWrapper({Key? key}) : super(key: key);
+
+  @override
+  State<MainWrapper> createState() => _MainWrapperState();
+}
+
+class _MainWrapperState extends State<MainWrapper> {
+  @override
+  void initState() {
+    super.initState();
+
+    BlocProvider.of<HomeBloc>(context).add(const LoadCwEvent('Tehran'));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (BuildContext context, HomeState state) {
+          if (state.cwStatus is CwLoading) {
+            return const Center(
+              child: Text('Loading ...'),
+            );
+          }
+          if (state.cwStatus is CwCompleted) {
+            return const Center(
+              child: Text('COMPLETED'),
+            );
+          }
+          if (state.cwStatus is CwError) {
+            return const Center(
+              child: Text('Error'),
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,23 @@
-import 'features/feature_weather/domain/use_cases/get_current_weather_usecase.dart';
-import 'features/feature_weather/data/repository/weather_repositoryimpl.dart';
-import 'features/feature_weather/data/data_source/remote/api_provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'core/widgets/main_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'features/feature_weather/presentation/bloc/home_bloc.dart';
 import 'locator.dart';
 
 void main() async {
-  //! Dependency injection with get_it
+  //! Dependency injection with get_it - init locator
   await setup();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    GetCurrentWeatherUseCase getCurrentWeatherUseCase =
-        GetCurrentWeatherUseCase(WeatherRepositoryImpl(ApiProvider()));
-
-    getCurrentWeatherUseCase('tehran');
-
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainWrapper(),
-    );
-  }
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => locator<HomeBloc>(),
+          ),
+        ],
+        child: const MainWrapper(),
+      ),
+    ),
+  );
 }
