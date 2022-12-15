9d746132ee6261fe0fc3cecfc2832f1b8192d7e7
Mahdi <mti.safdari@gmail.com>
2022-12-15 10:45:20 +0330

    add UseCase suggest city name

---
+++ lib/features/feature_weather/domain/use_cases/get_suggestion_city_usecase.dart
@@ -0,0 +1,13 @@
+import '../../../../core/use_case/use_case.dart';
+import '../../data/models/suggest_city_model.dart';
+import '../repository/weather_repository.dart';
+
+class GetSuggestionCityUseCase implements UseCase<List<Data>, String> {
+  final WeatherRepository _weatherRepository;
+
+  GetSuggestionCityUseCase(this._weatherRepository);
+  @override
+  Future<List<Data>> call(String params) {
+    return _weatherRepository.fetchSuggestData(params);
+  }
+}
