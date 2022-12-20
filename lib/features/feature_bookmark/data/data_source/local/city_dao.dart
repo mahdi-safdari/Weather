6dbda53b7a474c664c2be91fd584369c787d24d1
Mahdi <mti.safdari@gmail.com>
2022-12-20 20:38:54 +0330

    City Dao class

---
+++ lib/features/feature_bookmark/data/data_source/local/city_dao.dart
@@ -0,0 +1,18 @@
+import 'package:floor/floor.dart';
+
+import '../../../domain/entities/city_entity.dart';
+
+@dao
+abstract class CityDao {
+  @Query('SELECT * FROM City')
+  Future<List<City>> getAllCity();
+
+  @Query('SELECT * FROM City WHERE name = :name')
+  Future<City?> findCityByName(String name);
+
+  @insert
+  Future<void> insertCity(City param);
+
+  @Query('DELETE * FROM City WHERE name = :name')
+  Future<void> deleteCityByName(String name);
+}
