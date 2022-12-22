3bb2e595a600faf4a450e323f8c435753e7be747
Mahdi <mti.safdari@gmail.com>
2022-12-22 12:38:31 +0330

    locator database

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,14 +1,15 @@
-import 'package:clean_block_floor_lint_dio/features/feature_weather/domain/use_cases/get_forecast_weather_usecase.dart';
 import 'package:get_it/get_it.dart';
+import 'features/feature_bookmark/data/data_source/local/database.dart';
 import 'features/feature_weather/data/data_source/remote/api_provider.dart';
 import 'features/feature_weather/data/repository/weather_repositoryimpl.dart';
 import 'features/feature_weather/domain/repository/weather_repository.dart';
 import 'features/feature_weather/domain/use_cases/get_current_weather_usecase.dart';
+import 'features/feature_weather/domain/use_cases/get_forecast_weather_usecase.dart';
 import 'features/feature_weather/presentation/bloc/home_bloc.dart';
 
 GetIt locator = GetIt.instance;
 //! Dependency injection with get_it
-setup() {
+setup() async {
   //! API provider
   locator.registerSingleton<ApiProvider>(ApiProvider());
 
@@ -26,4 +27,7 @@ setup() {
 
   //! Bloc
   locator.registerSingleton<HomeBloc>(HomeBloc(locator(), locator()));
+
+  final database = $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  locator.registerSingleton<AppDatabase>(database as AppDatabase);
 }
