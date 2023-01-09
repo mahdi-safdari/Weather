83665bb0dd4cfbc3da4b2d1a33595cc6548c38ee
Mehdi Safdari <mti.safdari@gmail.com>
2023-01-09 16:48:39 +0330

    Bookmark City Bloc

--- lib/features/feature_bookmark/presentation/bloc/bookmark_bloc.dart
+++ lib/features/feature_bookmark/presentation/bloc/bookmark_bloc.dart
@@ -1,6 +1,10 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
+import '../../../../core/resources/data_state.dart';
+import '../../domain/use_cases/get_city_usecase.dart';
+import '../../domain/use_cases/save_city_usecase.dart';
 import 'get_city_status.dart';
 import 'save_city_status.dart';
 
@@ -8,13 +12,52 @@ part 'bookmark_event.dart';
 part 'bookmark_state.dart';
 
 class BookmarkBloc extends Bloc<BookmarkEvent, BookmarkState> {
-  BookmarkBloc()
-      : super(BookmarkState(
+  GetCityUseCase getCityUseCase;
+  SaveCityUseCase saveCityUseCase;
+  BookmarkBloc(
+    this.getCityUseCase,
+    this.saveCityUseCase,
+  ) : super(BookmarkState(
           saveCityStatus: SaveCityInitial(),
           getCityStatus: GetCityLoading(),
         )) {
-    on<BookmarkEvent>((event, emit) {
-      // TODO: implement event handler
+    //! get city by name event from database
+    on<GetCityByNameEvent>((event, emit) async {
+      //! emit loading state
+      emit(state.copyWith(newGetCityStatus: GetCityLoading()));
+
+      DataState dataState = await getCityUseCase(event.cityName);
+
+      //! emit complete state
+      if (dataState is DataSuccess) {
+        emit(
+            state.copyWith(newGetCityStatus: GetCityCompleted(dataState.data)));
+      }
+
+      //! emit error state
+      if (dataState is DataFailed) {
+        emit(state.copyWith(newGetCityStatus: GetCityError(dataState.error!)));
+      }
+    });
+
+    on<SaveCwEvent>((event, emit) async {
+      //! emit loading state
+      emit(state.copyWith(newSaveCityStatus: SaveCityLoading()));
+      DataState dataState = await getCityUseCase(event.name);
+
+      //! emit complete state
+      if (dataState is DataSuccess) {
+        emit(state.copyWith(
+            newSaveCityStatus: SaveCityCompleted(dataState.data)));
+      }
+      //! emit error state
+      if (dataState is DataFailed) {
+        emit(
+            state.copyWith(newSaveCityStatus: SaveCityError(dataState.error!)));
+      }
+    });
+    on<SaveCityInitialEvent>((event, emit) async {
+      emit(state.copyWith(newSaveCityStatus: SaveCityInitial()));
     });
   }
 }
--- lib/features/feature_bookmark/presentation/bloc/bookmark_event.dart
+++ lib/features/feature_bookmark/presentation/bloc/bookmark_event.dart
@@ -1,8 +1,26 @@
 part of 'bookmark_bloc.dart';
 
-abstract class BookmarkEvent extends Equatable {
-  const BookmarkEvent();
+@immutable
+abstract class BookmarkEvent {}
 
-  @override
-  List<Object> get props => [];
+class GetAllCityEvent extends BookmarkEvent {}
+
+class GetCityByNameEvent extends BookmarkEvent {
+  final String cityName;
+
+  GetCityByNameEvent(this.cityName);
+}
+
+class SaveCwEvent extends BookmarkEvent {
+  final String name;
+
+  SaveCwEvent(this.name);
+}
+
+class SaveCityInitialEvent extends BookmarkEvent {}
+
+class DeleteCityEvent extends BookmarkEvent {
+  final String name;
+
+  DeleteCityEvent(this.name);
 }
