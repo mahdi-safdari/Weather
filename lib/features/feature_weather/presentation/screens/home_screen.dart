c29bb122b950eefd89466367056d01881954b558
Mehdi Safdari <mti.safdari@gmail.com>
2023-02-15 14:31:05 +0330

    bloc completed & bookmark icon

--- lib/features/feature_weather/presentation/screens/home_screen.dart
+++ lib/features/feature_weather/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:clean_block_floor_lint_dio/features/feature_bookmark/presentation/bloc/bookmark_bloc.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -13,6 +14,7 @@ import '../../../../core/params/forecast_param.dart';
 import '../../data/models/forecast_days_model.dart';
 import '../../data/models/suggest_city_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../widgets/bookmark_icon.dart';
 import '../widgets/day_weather_view.dart';
 import 'package:flutter/material.dart';
 import '../../../../locator.dart';
@@ -50,44 +52,87 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(height: height * 0.02),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: width * 0.03),
-            child: TypeAheadField(
-              textFieldConfiguration: TextFieldConfiguration(
-                onSubmitted: (String prefix) {
-                  textEditingController.text = prefix;
-                  BlocProvider.of<HomeBloc>(context).add(LoadCwEvent(prefix));
-                },
-                controller: textEditingController,
-                style: DefaultTextStyle.of(context).style.copyWith(
-                      fontSize: 20,
-                      color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TypeAheadField(
+                    textFieldConfiguration: TextFieldConfiguration(
+                      onSubmitted: (String prefix) {
+                        textEditingController.text = prefix;
+                        BlocProvider.of<HomeBloc>(context)
+                            .add(LoadCwEvent(prefix));
+                      },
+                      controller: textEditingController,
+                      style: DefaultTextStyle.of(context).style.copyWith(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                      decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                        hintText: "Enter a City...",
+                        hintStyle: TextStyle(color: Colors.white),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
                     ),
-                decoration: const InputDecoration(
-                  contentPadding: EdgeInsets.fromLTRB(20, 0, 0, 0),
-                  hintText: "Enter a City...",
-                  hintStyle: TextStyle(color: Colors.white),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
+                    suggestionsCallback: (String pattern) {
+                      return getSuggestionCityUseCase(pattern);
+                    },
+                    itemBuilder: (BuildContext context, Data itemData) {
+                      return ListTile(
+                        leading: const Icon(Icons.location_on),
+                        title: Text(itemData.name!),
+                        subtitle:
+                            Text('${itemData.region}, ${itemData.country}'),
+                      );
+                    },
+                    onSuggestionSelected: (Data suggestion) {
+                      textEditingController.text = suggestion.name!;
+                      BlocProvider.of<HomeBloc>(context)
+                          .add(LoadCwEvent(suggestion.name!));
+                    },
                   ),
                 ),
-              ),
-              suggestionsCallback: (String pattern) {
-                return getSuggestionCityUseCase(pattern);
-              },
-              itemBuilder: (BuildContext context, Data itemData) {
-                return ListTile(
-                  leading: const Icon(Icons.location_on),
-                  title: Text(itemData.name!),
-                  subtitle: Text('${itemData.region}, ${itemData.country}'),
-                );
-              },
-              onSuggestionSelected: (Data suggestion) {
-                textEditingController.text = suggestion.name!;
-                BlocProvider.of<HomeBloc>(context)
-                    .add(LoadCwEvent(suggestion.name!));
-              },
+                const SizedBox(width: 10),
+                BlocBuilder<HomeBloc, HomeState>(
+                  buildWhen: (HomeState previous, HomeState current) {
+                    if (previous.cwStatus == current.cwStatus) {
+                      return false;
+                    }
+                    return true;
+                  },
+                  builder: (BuildContext context, HomeState state) {
+                    //! Show loading state for cw
+                    if (state.cwStatus is CwLoading) {
+                      return const CircularProgressIndicator();
+                    }
+                    //! Show Error state for cw
+                    if (state.cwStatus is CwError) {
+                      return IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.error,
+                            color: Colors.white, size: 35),
+                      );
+                    }
+                    //! Show completed state for cw
+                    if (state.cwStatus is CwCompleted) {
+                      final CwCompleted cwCompleted =
+                          state.cwStatus as CwCompleted;
+                      BlocProvider.of<BookmarkBloc>(context).add(
+                          GetCityByNameEvent(
+                              cwCompleted.currentCityEntity.name!));
+                      return BookMarkIcon(
+                        name: cwCompleted.currentCityEntity.name!,
+                      );
+                    }
+                    return Container();
+                  },
+                ),
+              ],
             ),
           ),
 
