99c2628da5b1addddd42b4bdc429adedaf357519
Mehdi Safdari <mti.safdari@gmail.com>
2023-02-24 09:05:22 +0330

    forcast weather 5 day ui

--- lib/features/feature_weather/presentation/screens/home_screen.dart
+++ lib/features/feature_weather/presentation/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:clean_block_floor_lint_dio/features/feature_bookmark/presentation/bloc/bookmark_bloc.dart';
+import 'package:clean_block_floor_lint_dio/features/feature_weather/data/models/forecast_days_model.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 import '../../../../core/utils/date_converter.dart';
+import '../../domain/entities/forecase_days_entity.dart';
 import '../../domain/use_cases/get_suggestion_city_usecase.dart';
 import '../../../../core/widgets/dot_loading_widget.dart';
 import '../../domain/entities/current_city_entity.dart';
@@ -9,11 +11,13 @@ import '../../../../core/widgets/app_background.dart';
 import '../../../../core/params/forecast_param.dart';
 import '../../data/models/suggest_city_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/fw_status.dart';
 import '../widgets/bookmark_icon.dart';
 import 'package:flutter/material.dart';
 import '../../../../locator.dart';
 import '../bloc/cw_status.dart';
 import '../bloc/home_bloc.dart';
+import '../widgets/day_weather_view.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -380,6 +384,64 @@ class _HomeScreenState extends State<HomeScreen>
                       ),
                       const SizedBox(height: 30),
                       //! forcast weather 5 day
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 100,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10.0),
+                            child: Center(
+                              child: BlocBuilder<HomeBloc, HomeState>(
+                                builder: (BuildContext context, state) {
+                                  /// show Loading State for Fw
+                                  if (state.fwStatus is FwLoading) {
+                                    return const DotLoadingWidget();
+                                  }
+
+                                  /// show Completed State for Fw
+                                  if (state.fwStatus is FwCompleted) {
+                                    /// casting
+                                    final FwCompleted fwCompleted =
+                                        state.fwStatus as FwCompleted;
+                                    final ForecastDaysEntity
+                                        forecastDaysEntity =
+                                        fwCompleted.forecastDaysEntity;
+                                    final List<ListElement> mainDaily =
+                                        forecastDaysEntity.list!;
+
+                                    return ListView.builder(
+                                      shrinkWrap: true,
+                                      scrollDirection: Axis.horizontal,
+                                      itemCount: 8,
+                                      itemBuilder: (
+                                        BuildContext context,
+                                        int index,
+                                      ) {
+                                        return DaysWeatherView(
+                                          list: mainDaily[index],
+                                        );
+                                      },
+                                    );
+                                  }
+
+                                  /// show Error State for Fw
+                                  if (state.fwStatus is FwError) {
+                                    final FwError fwError =
+                                        state.fwStatus as FwError;
+                                    return Center(
+                                      child: Text(fwError.messeage),
+                                    );
+                                  }
+
+                                  /// show Default State for Fw
+                                  return Container();
+                                },
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                       const SizedBox(height: 30),
                     ],
                   ),
--- lib/features/feature_weather/presentation/widgets/day_weather_view.dart
+++ lib/features/feature_weather/presentation/widgets/day_weather_view.dart
@@ -4,7 +4,7 @@ import 'package:clean_block_floor_lint_dio/features/feature_weather/data/models/
 import 'package:flutter/material.dart';
 
 class DaysWeatherView extends StatefulWidget {
-  final List<ListElement> list;
+  final ListElement list;
   const DaysWeatherView({super.key, required this.list});
 
   @override
@@ -55,20 +55,20 @@ class _DaysWeatherViewState extends State<DaysWeatherView>
                 child: Column(
                   children: [
                     Text(
-                      DateConverter.changeDtToDateTime(widget.list[0].dt),
+                      DateConverter.changeDtToDateTime(widget.list.dt),
                       style: const TextStyle(fontSize: 12, color: Colors.grey),
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top: 5.0),
                       child: AppBackground.setIconForMain(
-                        widget.list[0].weather![0].description,
+                        widget.list.weather![0].description,
                       ),
                     ),
                     Expanded(
                       child: Padding(
                         padding: const EdgeInsets.only(top: 5.0),
                         child: Text(
-                          "${widget.list[0].main!.temp!.round()}\u00B0",
+                          "${widget.list.main!.temp!.round()}\u00B0",
                           style: const TextStyle(
                             fontSize: 15,
                             color: Colors.white,
