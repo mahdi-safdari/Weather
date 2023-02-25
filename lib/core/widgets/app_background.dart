643a0a53fd3ac4c1e886b8b5c7b511e59eaebd8c
Mehdi Safdari <mti.safdari@gmail.com>
2023-02-25 13:33:27 +0330

    change background fix

--- lib/core/widgets/app_background.dart
+++ lib/core/widgets/app_background.dart
@@ -22,7 +22,6 @@ class AppBackground {
   static BoxDecoration decorationGradient() {
     final list = [
       [Color(0xff3f51b5), Color(0xfff44336)],
-      [Color(0xffff9800), Color(0xff2196f3)],
       [Color(0xffffeb3b), Color(0xff00bcd4)],
       [Color(0xff795548), Color(0xff2196f3)],
       [Color(0xff00bcd4), Color(0xff673ab7)],
@@ -40,7 +39,6 @@ class AppBackground {
       [Color(0xff03a9f4), Color(0xffe91e63)],
       [Color(0xff3f51b5), Color(0xff00bcd4)],
       [Color(0xff673ab7), Color(0xff009688)],
-      [Color(0xffcddc39), Color(0xff03a9f4)],
       [Color(0xff3f51b5), Color(0xff00bcd4)],
       [Color(0xffe91e63), Color(0xff03a9f4)],
       [Color(0xff2196f3), Color(0xff4caf50)],
